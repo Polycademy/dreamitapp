56b6f61370bce66f76482f420bc87ec10662800b
Roger Qiu <roger.qiu@polycademy.com>
2013-07-26 07:40:07 +1000

    working on the panel item, we have rollover!

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -28,6 +28,33 @@
 					</div>
 				</div>
 			</div>
+			<div class="item_panel">
+				<h3 class="item_header">Hacker News App</h3>
+				<div class="item_image_container">
+					<div class="item_rollover">
+						<button type="button">Share</button>
+					</div>
+					<img src="img/example_item_image.png" />
+				</div>
+				<div class="item_desc">
+					<p>An app to help read Hacker News on the mobile phone or ipad.</p>
+				</div>
+				<div class="item_meta row-fluid">
+					<em class="item_author">Roger Qiu</em>
+					<div class="item_actions span3">
+						<a class="item_comments" href="#">32</a>
+						<a class="item_likes" href="#">2</a>
+					</div>
+					<div class="item_tags span9">
+						<ul>
+							<li><a href="#">iphone</a></li>
+							<li><a href="#">ipad</a></li>
+							<li><a href="#">android</a></li>
+							<li><a href="#">programming</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
 		<aside class="control_panel span3">
 			<h3>Search</h3>
