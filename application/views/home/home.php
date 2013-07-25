f6e127322185a82ae757af219d2521812ee3e010
Roger Qiu <roger.qiu@polycademy.com>
2013-07-26 09:51:06 +1000

    pretty much finished the panel item

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -2,7 +2,7 @@
 	<div class="main_wrapper row" equalise-heights-dir=".wall, .control_panel">
 		<div class="wall span9">
 			<div class="item_panel">
-				<h3 class="item_header">Hacker News App</h3>
+				<h3 class="item_header"><a href="#">Hacker News App</a></h3>
 				<div class="item_image_container">
 					<div class="item_rollover">
 						<button type="button">Share</button>
@@ -12,40 +12,19 @@
 				<div class="item_desc">
 					<p>An app to help read Hacker News on the mobile phone or ipad.</p>
 				</div>
-				<div class="item_meta row-fluid">
-					<em class="item_author">Roger Qiu</em>
-					<div class="item_actions span3">
-						<a class="item_comments" href="#">32</a>
-						<a class="item_likes" href="#">2</a>
+				<div class="item_meta">
+					<span class="item_author"><a href="#">Roger Qiu</a></span>
+					<div class="item_actions">
+						<a class="item_feedback" href="#">
+							<span class="item_number">32</span>
+							<span class="item_icon fui-chat"></span>
+						</a>
+						<a class="item_likes" href="#">
+							<span class="item_number">42</span>
+							<span class="item_icon fui-heart"></span>
+						</a>
 					</div>
-					<div class="item_tags span9">
-						<ul>
-							<li><a href="#">iphone</a></li>
-							<li><a href="#">ipad</a></li>
-							<li><a href="#">android</a></li>
-							<li><a href="#">programming</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="item_panel">
-				<h3 class="item_header">Hacker News App</h3>
-				<div class="item_image_container">
-					<div class="item_rollover">
-						<button type="button">Share</button>
-					</div>
-					<img src="img/example_item_image.png" />
-				</div>
-				<div class="item_desc">
-					<p>An app to help read Hacker News on the mobile phone or ipad.</p>
-				</div>
-				<div class="item_meta row-fluid">
-					<em class="item_author">Roger Qiu</em>
-					<div class="item_actions span3">
-						<a class="item_comments" href="#">32</a>
-						<a class="item_likes" href="#">2</a>
-					</div>
-					<div class="item_tags span9">
+					<div class="item_tags">
 						<ul>
 							<li><a href="#">iphone</a></li>
 							<li><a href="#">ipad</a></li>
