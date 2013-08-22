4505f52959b5b844959b40396cb2864584281ddf
Roger Qiu <roger.qiu@polycademy.com>
2013-08-23 03:47:45 +1000

    disqus comments count works, just need to cache the results now

--- js/config.js
+++ js/config.js
@@ -7,6 +7,7 @@
 var dreamItAppConfig = {
 	apiKeys: {
 		addThis: 'ra-51f43b6702e72e9a',
-		disqusShortname: 'dreamitapp'
+		disqusShortname: 'dreamitapp',
+		disqusApiKey: 'Daq487qR6b6lPJn9ThTE301tYFr8OSrLB35emaoGiphps5XD1869gJ2h0h60JdJ9'
 	}
 };
