ca38e0d879002fa5fb9ec990ad5589cc8196a4de
Roger Qiu <roger.qiu@polycademy.com>
2013-09-01 21:46:03 +1000

    cleaning up about page

--- application/views/about/about.php
+++ application/views/about/about.php
@@ -1,11 +1,5 @@
 <script type="text/ng-template" id="about.html">
 	<div class="container">
 		<p>This is the about page</p>
-		<div>
-			<form ng-submit="doSomething()">
-				<label for="search"><span class="fui-search"></span></label>
-				<input id="search" name="search" type="text" ng-model="searchTag" placeholder="Search" ng-change="doSomething()"></input>
-			</form>
-		</div>
 	</div>
 </script>
