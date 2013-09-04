5feb0cae3fbc5a2ce1c176496ee3ca9c5aee189b
Roger Qiu <cmcdragonkai@gmail.com>
2013-09-04 18:30:48 +1000

    like resource finished, begin testing likes

--- application/controllers/like.php
+++ application/controllers/like.php
@@ -41,8 +41,9 @@ class Like extends CI_Controller{
 	}
 
 	/**
-	 * Updates a like for an idea. Basically +1 or -1. This toggles depending on if the user has clicked
-	 * on it before.
+	 * Updates a like for an idea. Basically +1 or -1. This toggles depending on if the user has liked it before.
+	 * This may return validation errors (singular) if the author id or idea id is incorrect.
+	 * Otherwise it would return a system error.
 	 * @param  Integer $id Id of the idea to toggle likes
 	 * @return Json
 	 */
@@ -52,10 +53,38 @@ class Like extends CI_Controller{
 		//currently hardcoded
 		$author_id = 1;
 
-		//+1 first, if that doesn't work, go -1
-		if(!$this->Like_model->up_one($id, $author_id)){
-			$this->Like_model->down_one($id, $author_id);
+		$has_liked = $this->Like_model->has_liked($id, $author_id)p;
+
+		if($has_liked){
+			$query = $this->Like_model->down_one($id, $author_id);
+		}else{
+			$query = $this->Like_model->up_one($id, $author_id);
 		}
+
+		if($query){
+
+			$content = $query;
+			$code = 'success';
+
+		}else{
+
+			$content = current($this->Tags_model->get_errors());
+			$code = key($this->Tags_model->get_errors());
+
+			if($code == 'validation_error'){
+				$this->output->set_status_header(400);
+			}elseif($code == 'system_error'){
+				$this->output->set_status_header(500);
+			}
+
+		}
+
+		$output = array(
+			'content'	=> $content,
+			'code'		=> $code,
+		);
+		
+		Template::compose(false, $output, 'json');
 
 	}
 
--- application/models/like_model.php
+++ application/models/like_model.php
@@ -2,10 +2,19 @@
 
 class Like_model extends CI_Model{
 
+	protected $errors;
+
 	public function __construct(){
+
 		parent::__construct();
+	
 	}
 
+	/**
+	 * Gets the like count for a single idea
+	 * @param  Integer $id Idea id
+	 * @return Integer|Boolean
+	 */
 	public function read($id){
 
 		$this->db->select('likes');
@@ -34,22 +43,155 @@ class Like_model extends CI_Model{
 
 	}
 
-	public function up_one($id, $author_id){
+	/**
+	 * Determines whether a particular user has liked an idea or not.
+	 * Checks whether like record exists for the user.
+	 * Run this before deciding to run up_one or down_one
+	 * @param  Integer  $id        Idea id
+	 * @param  Integer  $author_id User account id
+	 * @return boolean
+	 */
+	public function has_liked($id, $author_id){
 
-		//this needs to do 2 things.
-		//First add a record to the likes table (only if a record doesnt exist)
-		//if it already exists, we dont do anything
-		//Second update the likes
+		$query = $this->db->get_where('likes', array('ideaId' => $id, 'authorId' => $author_id));
 
+		if($query->num_rows() > 0){
+			return true;
+		}else{
+			return false;
+		}
 
 	}
 
+	/**
+	 * Increments the like count for one idea
+	 * @param  Integer $id        Idea id
+	 * @param  Integer $author_id User account id
+	 * @return String|Boolean     '+1' string if successful
+	 */
+	public function up_one($id, $author_id){
+
+		if(!$this->validate_idea_author_ids($id, $author_id)){
+			return false;
+		}
+
+		$this->db->trans_start();
+
+		//insert a new like record
+		$this->db->insert('likes', array(
+			'ideaId'	=> $id,
+			'authorId'	=> $author_id
+		));
+
+		//increase the like count for the idea
+		$this->db->set('likes', 'likes+1');
+		$this->db->where('id', $id);
+		$this->db->update('ideas');
+
+		$this->db->trans_complete();
+
+		if($this->db->trans_status() === FALSE){
+
+			$msg = $this->db->_error_message();
+			$num = $this->db->_error_number();
+			$last_query = $this->db->last_query();
+			
+			log_message('error', 'Problem increasing like count by one: ' . $msg . ' (' . $num . '), using this query: "' . $last_query . '"');
+			
+			$this->errors = array(
+				'system_error'	=> 'Problem increasing like count.',
+			);
+			
+			return false;
+
+		}
+
+		return '+1';
+
+	}
+
+	/**
+	 * Decrements the like count for one idea
+	 * @param  Integer $id        Idea id
+	 * @param  Integer $author_id User account id
+	 * @return String|Boolean     '-1' string if successful
+	 */
 	public function down_one($id, $author_id){
 
-		//remove a record (if it exists) on the likes table
-		//only if did exist, do we minus a like
-		//minus 1 on the likes
+		if(!$this->validate_idea_author_ids($id, $author_id)){
+			return false;
+		}
 
+		$this->db->trans_start();
+
+		//delete like record
+		$this->db->delete('likes', array('ideaId' => $id, 'authorId' => $author_id));
+
+		//increase the like count for the idea
+		$this->db->set('likes', 'likes-1');
+		$this->db->where('id', $id);
+		$this->db->update('ideas');
+
+		$this->db->trans_complete();
+
+		if($this->db->trans_status() === FALSE){
+
+			$msg = $this->db->_error_message();
+			$num = $this->db->_error_number();
+			$last_query = $this->db->last_query();
+			
+			log_message('error', 'Problem decreasing like count by one: ' . $msg . ' (' . $num . '), using this query: "' . $last_query . '"');
+			
+			$this->errors = array(
+				'system_error'	=> 'Problem decreasing like count.',
+			);
+			
+			return false;
+
+		}
+
+		return '-1';
+
+	}
+
+	/**
+	 * Gets the error messages
+	 * @return Array
+	 */
+	public function get_errors(){
+
+		return $this->errors;
+
+	}
+
+	/**
+	 * Validates whether both the idea id and author id are valid ids
+	 * @param  Integer $id        Idea id
+	 * @param  Integer $author_id User account id
+	 * @return Boolean
+	 */
+	protected function validate_idea_author_ids($id, $author_id){
+
+		//this relies on UserAccount from PolyAuth being setup, for now hardcoded true
+		return true;
+		/*
+
+		$idea_query = $this->db->get_where('ideas', array('id' => $id));
+		$author_query = $this->db->get_where('user_accounts', array('id' => $author_id));
+
+		if($idea_query->num_rows() > 0 AND $author_query->num_rows() > 0){
+
+			return true;
+
+		}
+
+		$this->errors = array(
+			'validation_error'	=> 'Idea or author does not exist.'
+		);
+
+		return false;
+
+		*/
 
 	}
 
