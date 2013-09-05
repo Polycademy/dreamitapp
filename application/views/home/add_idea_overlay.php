ab8de0a7263e181ded3ddfd4b6911f73027f17a1
Roger Qiu <cmcdragonkai@gmail.com>
2013-09-05 18:19:04 +1000

    added form actions and required attribute to add idea  overlay

--- application/views/home/add_idea_overlay.php
+++ application/views/home/add_idea_overlay.php
@@ -1,27 +1,29 @@
 <script type="text/ng-template" id="add_idea_overlay.html">
 	<div class="add_idea_container" overlay-dir>
+		<button class="idea_close" ng-click="closeOverlay()"><span class="fui-cross"></span></button>
 		<form class="form-horizontal" ng-submit="submitIdea()">
 			<fieldset>
 				<legend>Add a New Idea</legend>
 				<div class="control-group">
 					<label class="control-label" for="add_idea_title">Title:</label>
 					<div class="controls">
-						<input type="text" id="add_idea_title"></input>
+						<input type="text" id="add_idea_title" required></input>
 					</div>
 				</div>
 				<div class="control-group">
 					<label class="control-label" for="add_idea_short_description">Short Description:</label>
 					<div class="controls">
-						<input type="text" id="add_idea_short_description"></input>
+						<input type="text" id="add_idea_short_description" required></input>
+						<span class="help-block">This will be shown on the home page wall. Write up to 40 words.</span>
 					</div>
 				</div>
 				<div class="control-group">
-					<button class="btn btn-info">Add an Image</button>
+					<button class="add_idea_image_button btn btn-info" type="button">Add an Image</button>
 				</div>
 				<div class="control-group">
 					<label class="control-label" for="add_idea_description">Description:</label>
 					<div class="controls">
-						<textarea id="add_idea_description"></textarea>
+						<textarea id="add_idea_description" required></textarea>
 					</div>
 				</div>
 				<div class="control-group">
@@ -34,6 +36,10 @@
 						<input type="radio" name="add_idea_privacy" value="developers">
 						Developers Only
 					</label>
+				</div>
+				<div class="form-actions">
+					<button type="submit" class="btn btn-primary">Add Idea</button>
+					<button type="button" class="btn" ng-click="closeOverlay()">Cancel</button>
 				</div>
 			</fieldset>
 		</form>
