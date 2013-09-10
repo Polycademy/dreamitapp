38948b5b48feccced0e15571d28c4a7f764ab2bd
Roger Qiu <roger.qiu@polycademy.com>
2013-09-11 04:49:23 +1000

    filling out the about page

--- application/views/about/about.php
+++ application/views/about/about.php
@@ -1,50 +1,97 @@
 <script type="text/ng-template" id="about.html">
 	<article class="container about" pull-down-to-window-dir>
 		<header class="about_header page-header">
-			<h1>Welcome to Dream It App</h1>
+			<h1>Welcome to Dream it App</h1>
 		</header>
 		<div class="about_content">
 			<div class="about_intro">
+				<p class="lead text-center">Dream it App is a platform for showcasing your ideas for web, mobile or tablet applications, games or software. Build your audience here.</p>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. </p>
 				<p>Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. </p>
 				<p>Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. </p>
-				<p>Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. </p>
-				<p>Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. </p>
 			</div>
 			<div class="about_features">
-				
+				<p class="lead text-center">Here's how we can help:</p>
+				<div class="row">
+					<div class="span4">
+						<h3>Add Ideas</h3>
+						<img class="feature_image" src="img/apple-touch-icon-57x57-precomposed.png" />
+						<p>You can add ideas, and use markdown for description. You can also set the privacy settings. One image is allowed for the main image.</p>
+					</div>
+					<div class="span4">
+						<h3>Gain Feedback</h3>
+						<img class="feature_image" src="img/apple-touch-icon-57x57-precomposed.png" />
+						<p>Check the comments and likes. Get your friends to like them, the most popular ones goes to top.</p>
+					</div>
+					<div class="span4">
+						<h3>Contact Developers</h3>
+						<img class="feature_image" src="img/apple-touch-icon-57x57-precomposed.png" />
+						<p>Developers can get in contact with you if your idea is interesting, or you can browse developer profiles.</p>
+					</div>
+				</div>
 			</div>
 			<div class="about_team">
+				<p class="lead text-center">Check out the team behind this project:</p>
+				<div class="row">
+					<div class="span6">
+						<div class="pull-left">
+							<img class="feature_image" src="img/apple-touch-icon-57x57-precomposed.png" />
+						</div>
+						<p>Stephen Kiley</p>
+					</div>
+					<div class="span6">
+						<div class="pull-left">
+							<img class="feature_image" src="img/apple-touch-icon-57x57-precomposed.png" />
+						</div>
+						<p>Roger Qiu</p>
+					</div>
+				</div>
 			</div>
 			<div class="contact_us">
-				<form class="form-horizontal">
-					<fieldset>
-						<legend>Contact Us</legend>
-						<div class="control-group">
-							<label class="control-label" for="contact_message">Message:</label>
-							<div class="controls">
-								<textarea 
-									id="contact_message" 
-									class="input-xxlarge" 
-									rows="6"
-								></textarea>
+				<p class="lead text-center">Contact Us:</p>
+				<div class="row">
+					<div class="span6">
+						<form class="form-horizontal">
+							<div class="control-group">
+								<label class="control-label" for="contact_message">Message:</label>
+								<div class="controls">
+									<textarea 
+										id="contact_message" 
+										class="input-block-level" 
+										rows="6"
+									></textarea>
+								</div>
 							</div>
-						</div>
-						<div class="control-group">
-							<label class="control-label" for="contact_email">Email:</label>
-							<div class="controls">
-								<input 
-									id="contact_email" 
-									class="input-xxlarge" 
-									type="email" 
-								/>
+							<div class="control-group">
+								<label class="control-label" for="contact_email">Email:</label>
+								<div class="controls">
+									<input 
+										id="contact_email" 
+										class="input-block-level" 
+										type="email" 
+									/>
+								</div>
 							</div>
-						</div>
-						<div class="form-actions">
-							<button type="submit" class="btn btn-primary">Send</button>
-						</div>
-					</fieldset>
-				</form>
+							<div class="form-actions">
+								<button type="submit" class="btn btn-primary">Send</button>
+							</div>
+						</form>
+					</div>
+					<div class="span6">
+						<address>
+							<strong>Dream it App</strong><br />
+							795 Folsom Street<br />
+							Canberra, ACT, Australia 2207<br />
+							<abbr title="Phone">P:</abbr> (123) 456-7890<br />
+							<a href="http://www.google.com/recaptcha/mailhide/d?k=01ebDHQgYSQIgO98BBkNl3Iw==&amp;c=3jlSO61bF3OS40LW0BlSJfhS_gKpl9U6QfBANh2YGXE=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501ebDHQgYSQIgO98BBkNl3Iw\75\75\46c\0753jlSO61bF3OS40LW0BlSJfhS_gKpl9U6QfBANh2YGXE\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="Reveal this e-mail address">deve...@dreamitapp.com</a>
+						</address>
+					</div>
+				</div>
+			</div>
+			<div class="about_minor text-center">
+				<small class="muted">&copy; Dream it App 2013</small>
+				<br />
+				<small class="muted">Dream it App was handmade at <a href="http://polycademy.com">Polycademy</a></small>
 			</div>
 		</div>
 	</article>
