28f266d14434785706e3eb2cf15b2a6cdaed5016
Roger Qiu <roger.qiu@polycademy.com>
2013-09-11 04:19:27 +1000

    preliminary about page

--- application/views/about/about.php
+++ application/views/about/about.php
@@ -1,21 +1,51 @@
 <script type="text/ng-template" id="about.html">
-	<div class="container">
-		<p>This is the about page</p>
-	</div>
-	<span 
-		share-this-dir 
-		share-this-publisher-id="{{dreamItAppConfig.apiKeys.shareThis}}" 
-		share-this-url="{{'http://dreamitapp.com'}}" 
-		share-this-title="{{'fgfhgfh'}}"
-	>Share
-	</span>
-	<span 
-		share-this-dir 
-		share-this-publisher-id="{{dreamItAppConfig.apiKeys.shareThis}}" 
-		share-this-url="{{'http://dreamitapp.com'}}" 
-		share-this-title="{{'fgfhfdgdfgdfgfdgfdgdsfgsfdhfgdhfghgfgfh'}}" 
-		share-this-summary="{{'this is a summary'}}" 
-		share-this-image="{{'http://upload.wikimedia.org/wikipedia/commons/thumb/2/26/YellowLabradorLooking_new.jpg/260px-YellowLabradorLooking_new.jpg'}}"
-	>Share
-	</span>
+	<article class="container about" pull-down-to-window-dir>
+		<header class="about_header page-header">
+			<h1>Welcome to Dream It App</h1>
+		</header>
+		<div class="about_content">
+			<div class="about_intro">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. </p>
+				<p>Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. </p>
+				<p>Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. </p>
+				<p>Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. </p>
+				<p>Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. </p>
+			</div>
+			<div class="about_features">
+				
+			</div>
+			<div class="about_team">
+			</div>
+			<div class="contact_us">
+				<form class="form-horizontal">
+					<fieldset>
+						<legend>Contact Us</legend>
+						<div class="control-group">
+							<label class="control-label" for="contact_message">Message:</label>
+							<div class="controls">
+								<textarea 
+									id="contact_message" 
+									class="input-xxlarge" 
+									rows="6"
+								></textarea>
+							</div>
+						</div>
+						<div class="control-group">
+							<label class="control-label" for="contact_email">Email:</label>
+							<div class="controls">
+								<input 
+									id="contact_email" 
+									class="input-xxlarge" 
+									type="email" 
+								/>
+							</div>
+						</div>
+						<div class="form-actions">
+							<button type="submit" class="btn btn-primary">Send</button>
+						</div>
+					</fieldset>
+				</form>
+			</div>
+		</div>
+	</article>
 </script>
