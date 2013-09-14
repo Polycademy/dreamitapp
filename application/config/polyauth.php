0b781da8984c233702ff20fe02040f61826cd54b
Roger Qiu <roger.qiu@polycademy.com>
2013-09-15 08:29:07 +1000

    configuring for polyauth, make sure to test out on localhost urls

--- application/config/polyauth.php
+++ application/config/polyauth.php
@@ -3,6 +3,33 @@
 //PolyAuth Options
 
 $config['polyauth'] = array(
-	'email'				=> true,
 	'session_handler'	=> new PolyAuth\Sessions\EncryptedSessionHandler($this->config['encryption_key']),
+	//EMAIL SETTINGS
+	'email'					=> true,
+	'email_smtp'			=> true,
+	'email_host'			=> 'smtp.mandrillapp.com',
+	'email_auth'			=> true,
+	'email_username'		=> $_ENV['secrets']['mandrill_user'],
+	'email_password'		=> $_ENV['secrets']['mandrill_key'],
+	'email_smtp_secure'		=> 'tls',
+	'email_from'			=> $this->config['sitemeta']['email'],
+	'email_from_name'		=> $this->config['sitemeta']['title'],
+	//LOGIN SETTINGS
+	'login_identity'		=> 'username',
+	//REGISTRATION SETTINGS
+	'reg_activation'		=> 'email',
+	//EXTERNAL PROVIDER SETTINGS
+	'external_providers'	=> array(
+		'facebook'	=> array(
+			'key'			=> $_ENV['secrets']['facebook_id'],
+			'secret'		=> $_ENV['secrets']['facebook_token'],
+			'scope'			=> array('email'),
+			'callback_url'	=> 'http://dreamitapp.com/signin', //change this to http://dreamitapp.com/signin in production
+			'identifier'	=> array(
+				'api'	=> 'me',
+				'key'	=> 'email',
+				'type'	=> 'email',
+			)
+		)
+	)
 );
