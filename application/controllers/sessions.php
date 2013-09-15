87d3af0e6643e026ca4c12dd8f6489eeb591271b
Roger Qiu <roger.qiu@polycademy.com>
2013-09-16 05:40:25 +1000

    working on sessions backend

--- application/controllers/sessions.php
+++ application/controllers/sessions.php
@@ -3,27 +3,61 @@
 class Sessions extends CI_Controller{
 
 	public function __construct(){
+
 		parent::__construct();
+		$this->load->model('Sessions_model');
+	
 	}
 
 	// public function index(){
 
 	// }
 	
-	// public function show($id){
+	public function show($id){
 		
-	// }
+	}
 
-	// public function create(){
+	public function create(){
 
-	// }
+		$data = $this->input->json(false);
 
-	// public function update($id){
+		$query = $this->Session_model->create($data);
+		
+		if($query){
+		
+			$this->output->set_status_header('201');
+			$content = $query; //resource id
+			$code = 'success';
+		
+		}else{
+		
+			
+			$content = current($this->Sessions_model->get_errors());
+			$code = key($this->Sessions_model->get_errors());
+			
+			if($code == 'validation_error'){
+				$this->output->set_status_header(400);
+			}elseif($code == 'system_error'){
+				$this->output->set_status_header(500);
+			}
+			
+		}
+		
+		$output = array(
+			'content'	=> $content,
+			'code'		=> $code,
+		);
+		
+		Template::compose(false, $output, 'json');
 
-	// }
+	}
 
-	// public function delete($id){
+	public function update($id){
 
-	// }
+	}
+
+	public function delete($id){
+
+	}
 
 }
