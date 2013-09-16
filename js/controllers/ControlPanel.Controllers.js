f02624c61a7f37585482204b914415021c95d4ff
Roger Qiu <roger.qiu@polycademy.com>
2013-09-16 11:07:38 +1000

    forgot about the validation errors for sign in

--- js/controllers/ControlPanel.Controllers.js
+++ js/controllers/ControlPanel.Controllers.js
@@ -272,6 +272,16 @@ define(['angular', 'lodash'], function(angular, _){
 
 					}, function(response){
 
+						$scope.validationErrors = [];
+						if(response.data.code = 'validation_error'){
+							//the content would be an object of fields to errors
+							for(var key in response.data.content){
+								$scope.validationErrors.push(response.data.content[key]); 
+							}
+						}else{
+							$scope.validationErrors = [response.data.content];
+						}
+
 					});
 
 				};
