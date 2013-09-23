6b0dad876419f67950cb80f1bf3daacbb934f2ef
Roger Qiu <roger.qiu@polycademy.com>
2013-09-24 04:06:36 +1000

    set admin to be true for the npm global installer

--- npm_global_installer.js
+++ npm_global_installer.js
@@ -1,13 +1,16 @@
 require('package-script').spawn([
 	{
+		admin: true,
 		command: "npm",
 		args: ["install", "-g", "grunt-cli"]
 	},
 	{
+		admin: true,
 		command: "npm",
 		args: ["install", "-g", "bower"]
 	},
 	{
+		admin: true,
 		command: "npm",
 		args: ["install", "-g", "requirejs"]
 	}
