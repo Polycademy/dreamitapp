b779e5b0e0ca8e0e1e77a0fc820efd1ce63abc0e
Roger Qiu <roger.qiu@polycademy.com>
2013-09-24 04:01:35 +1000

    npm now installs global dependencies of grunt-cli, bower and requirejs

---
+++ npm_global_installer.js
@@ -0,0 +1,14 @@
+require('package-script').spawn([
+	{
+		command: "npm",
+		args: ["install", "-g", "grunt-cli"]
+	},
+	{
+		command: "npm",
+		args: ["install", "-g", "bower"]
+	},
+	{
+		command: "npm",
+		args: ["install", "-g", "requirejs"]
+	}
+]);
