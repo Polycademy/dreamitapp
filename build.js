7f4fd9d24bcc8f114e1c6edeb05664204c0816be
Roger Qiu <roger.qiu@polycademy.com>
2013-09-24 04:46:35 +1000

    preparing for requirejs optimisation

---
+++ build.js
@@ -0,0 +1,18 @@
+/**
+ * RequireJS Optimiser Build Configuration.
+ * This is ran from Grunt.
+ */
+({
+	baseUrl: 'build/js/', //base directory of source
+	dir: 'build/js/', //base directory of the build output
+	mainConfigFile: 'build/js/bootstrap.js', //import runtime config
+	keepBuildDir: true, //build dir should not be deleted
+	skipDirOptimize: true, //we're only optimising a single file here
+	findNestedDependencies: true, //could be false?? we should find nested dependencies!
+	name: 'bootstrap', //this is the file we're going to optimise
+	out: 'bootstrap.js', //this is final optimised file
+	paths: { //empty are for any scripts loaded from a CDN
+		'sharethis': 'empty:',
+		'filepicker': 'empty:'
+	}
+})
--- js/bootstrap.js
+++ js/bootstrap.js
@@ -9,64 +9,25 @@ requirejs.config({
 	waitSeconds: 12,
 	paths: {
 		//FRAMEWORK
-		'jquery': [
-			// '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
-			'../bower_components/jquery/jquery.min'
-		],
-		'bootstrap': [
-			'//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.2/js/bootstrap.min',
-			'lib/bootstrap.min'
-		],
-		'angular': [
-			// '//ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min',
-			'../bower_components/angular/angular.min'
-		],
-		'angular-resource': [
-			// '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.0.7/angular-resource.min',
-			'../bower_components/angular-resource/angular-resource.min'
-		],
-		'angular-cookies': [
-			// '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.0.7/angular-cookies.min',
-			'../bower_components/angular-cookies/angular-cookies.min'
-		],
-		'angular-sanitize': [
-			// '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.0.7/angular-sanitize.min',
-			'../bower_components/angular-sanitize/angular-sanitize.min'
-		],
-		'angular-ui': [
-			// '//cdnjs.cloudflare.com/ajax/libs/angular-ui/0.4.0/angular-ui.min',
-			'../bower_components/angular-ui/build/angular-ui.min'
-		],
-		'angular-bootstrap': [
-			// '//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.4.0/ui-bootstrap-tpls.min',
-			'../bower_components/angular-bootstrap/ui-bootstrap-tpls.min'
-		],
-		'angular-ui-router': [
-			// '//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.0.1/angular-ui-router.min',
-			'../bower_components/angular-ui-router/release/angular-ui-router.min'
-		],
-		'angular-cache': [
-			// 'https://raw.github.com/jmdobry/angular-cache/master/dist/angular-cache-1.0.0-rc.1.min',
-			'../bower_components/angular-cache/src/angular-cache'
-		],
+		'jquery': '../bower_components/jquery/jquery.min',
+		'bootstrap': 'lib/bootstrap.min',
+		'angular': '../bower_components/angular/angular.min',
+		'angular-resource': '../bower_components/angular-resource/angular-resource.min',
+		'angular-cookies': '../bower_components/angular-cookies/angular-cookies.min',
+		'angular-sanitize': '../bower_components/angular-sanitize/angular-sanitize.min',
+		'angular-ui': '../bower_components/angular-ui/build/angular-ui.min',
+		'angular-bootstrap': '../bower_components/angular-bootstrap/ui-bootstrap-tpls.min',
+		'angular-ui-router': '../bower_components/angular-ui-router/release/angular-ui-router.min',
+		'angular-cache': '../bower_components/angular-cache/src/angular-cache',
 		'angulartics': '../bower_components/angulartics/src/angulartics',
 		'angulartics-google-analytics': '../bower_components/angulartics/src/angulartics-google-analytics',
 		//ANGULAR UI SELECT2
-		'select2': [
-			// '//cdn.jsdelivr.net/select2/3.4.2/select2.min',
-			'../bower_components/select2/select2.min'
-		],
+		'select2': '../bower_components/select2/select2.min',
 		'angular-ui-select2': '../bower_components/angular-ui-select2/src/select2',
 		//SHIMS AND UTILITIES
 		'domReady': 'lib/domReady.min',
-		'lodash': [
-			// '//cdnjs.cloudflare.com/ajax/libs/lodash.js/1.3.1/lodash.min',
-			'../bower_components/lodash/dist/lodash.min'
-		],
-		'es5-shim': [
-			// '//cdn.jsdelivr.net/es5.shim/2.1.0/es5-shim.min',
-			'../bower_components/es5-shim/es5-shim.min'
-		],
+		'lodash': '../bower_components/lodash/dist/lodash.min',
+		'es5-shim':	'../bower_components/es5-shim/es5-shim.min',
 		'es6-shim': '../bower_components/es6-shim/es6-shim',
 		'json3': '../bower_components/json3/lib/json3.min',
 		//JQUERY PLUGINS
@@ -82,15 +43,9 @@ requirejs.config({
 		'imagesLoaded': '../bower_components/imagesloaded/imagesloaded',
 		'masonry': '../bower_components/masonry/masonry',
 		//SHARETHIS
-		'sharethis': [
-			'//w.sharethis.com/button/buttons',
-			'lib/buttons'
-		],
+		'sharethis': '//w.sharethis.com/button/buttons',
 		//FILEPICKER
-		'filepicker': [
-			'//api.filepicker.io/v1/filepicker',
-			'lib/filepicker'
-		]
+		'filepicker': '//api.filepicker.io/v1/filepicker',
 	},
 	shim: {
 		'bootstrap': {
