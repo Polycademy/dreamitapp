603a9fbc389b8a9317bc073ccede6bdd41c034b1
Roger Qiu <roger.qiu@polycademy.com>
2013-12-30 15:24:56 +1100

    changed signin or signup message

--- application/views/control_panel/signin_or_signup_modal.php
+++ application/views/control_panel/signin_or_signup_modal.php
@@ -1,7 +1,7 @@
 <script type="text/ng-template" id="signin_or_signup_modal.html">
 	<div class="signin_container">
 		<header class="signin_header page-header">
-			<h3>Sign In or Sign Up to Proceed</h3>
+			<h3>To comment or like an idea, please register or sign in if you are already a member</h3>
 			<button class="signin_close overlay_close" ng-click="closeOverlay()">
 				<span class="fui-cross"></span>
 			</button>
